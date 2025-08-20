12ac347f29b2b1e7021d67565c1eed7c52541f2e
SUNGIN99 <99_insung@naver.com>
2025-08-21 01:31:12 +0900

    test: 인기 상품 단순 조회 캐시 테스트 작성

--- src/test/java/kr/hhplus/be/server/cache/TopProductRankWithRedisTest.java
+++ src/test/java/kr/hhplus/be/server/cache/TopProductRankWithRedisTest.java
@@ -2,17 +2,21 @@ package kr.hhplus.be.server.cache;
 
 import kr.hhplus.be.server.TestDataSourceProxyConfig;
 import kr.hhplus.be.server.TestcontainersConfiguration;
+import kr.hhplus.be.server.domain.order.application.facade.OrderFacade;
 import kr.hhplus.be.server.domain.product.adapter.cache.TopProductCacheRepository;
+import kr.hhplus.be.server.domain.product.application.facade.ProductFacade;
 import kr.hhplus.be.server.domain.product.command.ProductRankingDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -40,38 +44,94 @@ public class TopProductRankWithRedisTest {
     @Autowired
     StringRedisTemplate stringRedisTemplate;
 
-    private List<ProductRankingDto.ProductRankEntry> selectZsetCache(String key){
-        Set<ZSetOperations.TypedTuple<String>> tuples = stringRedisTemplate.opsForZSet()
-                .rangeWithScores(key, 0, -1);
+    @Nested
+    class RedisScoringTest{
+        private List<ProductRankingDto.ProductRankEntry> selectZsetCache(String key){
+            Set<ZSetOperations.TypedTuple<String>> tuples = stringRedisTemplate.opsForZSet()
+                    .rangeWithScores(key, 0, -1);
 
-        List<ProductRankingDto.ProductRankEntry> result = new ArrayList<>();
-        if (tuples != null) {
-            tuples.forEach(t -> {
-                Long member = Long.valueOf(t.getValue());
-                int score  = t.getScore().intValue();
+            List<ProductRankingDto.ProductRankEntry> result = new ArrayList<>();
+            if (tuples != null) {
+                tuples.forEach(t -> {
+                    Long member = Long.valueOf(t.getValue());
+                    int score  = t.getScore().intValue();
 
-                result.add(new ProductRankingDto.ProductRankEntry(member, score));
-            });
+                    result.add(new ProductRankingDto.ProductRankEntry(member, score));
+                });
+            }
+
+            return result;
         }
 
-        return result;
+        @Test
+        void ZSET_스코어_변경_확인(){
+            List<ProductRankingDto.ProductItemForRank> orderItems = new ArrayList<>();
+            int count = 10;
+            for(int i = 1; i<= count; i++){
+                orderItems.add(new ProductRankingDto.ProductItemForRank((long) i, i));
+            }
+
+            cacheRepository.increaseTodayRankScores(orderItems);
+
+            List<ProductRankingDto.ProductRankEntry> result = selectZsetCache("zs:rank:product:realtime" + ":" + LocalDate.now());
+
+            assertThat(result).isNotEmpty();
+            assertThat(result).hasSize(count);
+
+            //result.forEach(System.out::println);
+        }
     }
 
-    @Test
-    void 주문_성공_후_ZSET_스코어_변경_확인(){
-        List<ProductRankingDto.ProductItemForRank> orderItems = new ArrayList<>();
-        int count = 10;
-        for(int i = 1; i<= count; i++){
-            orderItems.add(new ProductRankingDto.ProductItemForRank((long) i, i));
+    @Nested
+    class RedisScoreWithRankTest{
+        void testData(){
+            LocalDate[] range = new LocalDate[]{
+                    LocalDate.now().minusDays(1),
+                    LocalDate.now().minusDays(2),
+                    LocalDate.now().minusDays(3),
+            };
+
+            for(int day = 0; day<3; day++){
+                List<ProductRankingDto.ProductItemForRank> orderItems = new ArrayList<>();
+                int count = 10;
+                for(int i = 1; i<= count; i++){
+                    orderItems.add(new ProductRankingDto.ProductItemForRank((long) i, i * (day+1)));
+                }
+
+                try (var mocked = Mockito.mockStatic(LocalDate.class)) {
+                    mocked.when(LocalDate::now).thenReturn(range[day]);
+                    cacheRepository.increaseTodayRankScores(orderItems);
+                }
+            }
         }
 
-        cacheRepository.increaseTodayRankScores(orderItems);
+        @Test
+        void 지난_3일_인기상품_랭킹_조회(){
+            testData();
 
-        List<ProductRankingDto.ProductRankEntry> result = selectZsetCache("zs:rank:product:realtime" + ":" + LocalDate.now());
+            List<ProductRankingDto.ProductRankEntry> rankScores = cacheRepository.topFiveForThreeDaysZset();
 
-        assertThat(result).isNotEmpty();
-        assertThat(result).hasSize(count);
+            assertThat(rankScores).isNotEmpty();
+            assertThat(rankScores).hasSize(5);
 
-        //result.forEach(System.out::println);
+            rankScores.forEach(System.out::println);
+        }
     }
+
+    @Autowired
+    OrderFacade orderFacade;
+
+    @Autowired
+    ProductFacade productFacade;
+
+    @Sql(scripts = {
+            "classpath:sql/cleanup.sql",
+            "classpath:sql/schema.sql",
+            "classpath:sql/cache/august_100_data.sql"
+    }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    class OrderRankIntegrationTest{
+
+    }
+
+
 }
