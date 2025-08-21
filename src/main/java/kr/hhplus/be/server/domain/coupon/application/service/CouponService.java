65a542d729b090cdc4c2f7d9f6629b63207ee561
SUNGIN99 <99_insung@naver.com>
2025-08-22 04:20:03 +0900

    dev: 쿠폰 서비스 클레임 함수 작성(쿠폰 발급 요청 및 폴링 처리 기능)

--- src/main/java/kr/hhplus/be/server/domain/coupon/application/service/CouponService.java
+++ src/main/java/kr/hhplus/be/server/domain/coupon/application/service/CouponService.java
@@ -4,20 +4,29 @@ import kr.hhplus.be.server.config.aop.lock.DistributedLock;
 import kr.hhplus.be.server.config.aop.lock.LockType;
 import kr.hhplus.be.server.config.aop.lock.Resource;
 import kr.hhplus.be.server.config.aop.lock.ResourceKey;
+import kr.hhplus.be.server.domain.coupon.adapter.cache.CouponCacheKeyProvider;
+import kr.hhplus.be.server.domain.coupon.adapter.cache.CouponCacheQueue;
+import kr.hhplus.be.server.domain.coupon.adapter.cache.CouponCacheRepository;
+import kr.hhplus.be.server.domain.coupon.adapter.cache.CouponIssuedCacheRepository;
 import kr.hhplus.be.server.domain.coupon.application.Coupon;
 import kr.hhplus.be.server.domain.coupon.application.CouponIssue;
 import kr.hhplus.be.server.common.exception.coupon.InvalidCouponException;
 import kr.hhplus.be.server.domain.coupon.application.generator.CouponCodeGenerator;
 import kr.hhplus.be.server.domain.coupon.application.repository.CouponIssueRepository;
 import kr.hhplus.be.server.domain.coupon.application.repository.CouponRepository;
+import kr.hhplus.be.server.domain.coupon.controller.dto.CouponClaimCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Objects;
+import java.util.Optional;
+
+import static kr.hhplus.be.server.domain.coupon.adapter.cache.CouponCacheKeyProvider.CouponClaimStatus.*;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +35,10 @@ public class CouponService {
     private final CouponIssueRepository couponIssueRepository;
     private final CouponCodeGenerator codeGenerator;
 
+    private final CouponCacheRepository couponCacheRepository;
+    private final CouponIssuedCacheRepository couponIssuedCacheRepository;
+    private final CouponCacheQueue couponCacheQueue;
+
     public List<Coupon> getValidCoupons(){
         return couponRepository.findValidCouponList(LocalDateTime.now(), 0L);
     }
@@ -33,6 +46,52 @@ public class CouponService {
     public Coupon getCoupon(Long couponId){
         return couponRepository.findById(couponId)
                 .orElseThrow(() -> new NoSuchElementException("존재하지 않는 쿠폰 입니다."));
+    }
+
+    // 유저 쿠폰 발급 큐 요청
+    public CouponClaimCommand.CouponClaimResponse couponClaim(Long couponId, Long userId, String couponCode){
+        Long remains = couponCacheRepository.getRemainingStock(couponId);
+        CouponClaimCommand.CouponClaimResponse response = null;
+        if(remains <= 0){
+            throw new InvalidCouponException("FAILED TO ISSUE COUPON");
+        }else{
+            CouponCacheKeyProvider.CouponClaimStatus issuedStatus = couponIssuedCacheRepository.getClaimStatus(couponId, userId).get();
+
+            if(issuedStatus.equals(ISSUED)){
+                if(couponCode.isBlank()){
+                    throw new InvalidCouponException("INVALID COUPON CODE");
+                }
+                // 이미 발급된 쿠폰 입니다.
+                // 쿠폰 발급 캐시 데이터 전송 or 쿠폰 발급 DB 데이터 캐싱 후 데이터 전송
+                Optional<CouponIssue> iss = couponIssuedCacheRepository.getIssuedCoupon(couponId, userId);
+
+                if(iss.isEmpty()){
+                    iss = couponIssueRepository.findByCouponCode(couponCode);
+                    couponIssuedCacheRepository.cacheCouponIssuedData(couponId, userId, iss.get(), Duration.ofDays(7));
+                }
+                response = CouponClaimCommand.issued(iss.get());
+            }else if(issuedStatus.equals(FAILED)){
+                // 발급에 실패하였습니다.
+                throw new InvalidCouponException("FAILED TO ISSUE COUPON");
+            }else if(issuedStatus.equals(WAITED) || issuedStatus.equals(PROCESSING)){
+                // 현재 발급 중입니다. 앞선 대기 순위 n 번 째
+                Long rank = couponCacheQueue.getRank(couponId, userId);
+                response = CouponClaimCommand.ranked(couponId, userId, rank);
+            }else if(issuedStatus.equals(INIT)){
+                // todo: DB에 해당 유저의 발급내역이 있는지 확인해준 후 진행
+                //couponIssueRepositiory.findByCouponIdAndUserId
+
+                boolean issued = couponCacheQueue.enqueueClaim(couponId, userId);
+                if(issued){
+                    // 발급 요청이 정상적으로 진행되었습니다.
+                    response = CouponClaimCommand.init(couponId, userId);
+                }else{
+                    throw new InvalidCouponException("FAILED TO ISSUE COUPON");
+                }
+            }
+
+            return response;
+        }
     }
 
     @DistributedLock(
